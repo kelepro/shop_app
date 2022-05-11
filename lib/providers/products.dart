99edee5a551a1d2758170c5a55ca9ce0c68260b1
Kelemen Ervin <kelepro17@gmail.com>
2022-05-11 10:25:05 +0300

    Upload files to shop_app repo

---
+++ lib/providers/products.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+import './product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Smartphone Note 23 Pro',
+      description: 'Buy Note23 Pro Unlocked Smartphone, 6.82'
+          ' Full Screen Mobile Phone, 216G Dual Standby Phone',
+      price: 29.99,
+      imageUrl:
+          'https://www.ubuy.vn/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNjFFQ3dELTJLSkwuX0FDX1NMMTUwMF8uanBn.jpg',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Smartphone OnePlus 5G',
+      description:
+          'Best smartphone 2019: iPhone, OnePlus, Samsung and Huawei compared and ranked.',
+      price: 59.99,
+      imageUrl:
+          'https://i.guim.co.uk/img/media/2ce8db064eabb9e22a69cc45a9b6d4e10d595f06/392_612_4171_2503/master/4171.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=45b5856ba8cd83e6656fbe5c166951a4',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
+    Product(
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
+    Product(
+      id: 'p5',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+    Product(
+      id: 'p6',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+    Product(
+      id: 'p7',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+    Product(
+      id: 'p8',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+    Product(
+      id: 'p9',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+    Product(
+      id: 'p10',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+    Product(
+      id: 'p11',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+    Product(
+      id: 'p12',
+      title: 'Other product',
+      description: 'Prepare any meal you want.',
+      price: 99.99,
+      imageUrl:
+          'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png',
+    ),
+  ];
+  // var _showFavoritesOnly = false;
+
+  List<Product> get items {
+    // if (_showFavoritesOnly) {
+    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
+    // }
+    return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  // void showFavoritesOnly() {
+  //   _showFavoritesOnly = true;
+  //   notifyListeners();
+  // }
+
+  // void showAll() {
+  //   _showFavoritesOnly = false;
+  //   notifyListeners();
+  // }
+
+  void addProduct() {
+    // _items.add(value);
+    notifyListeners();
+  }
+}
